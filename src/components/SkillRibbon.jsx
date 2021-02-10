97a468e4c88d0f5d46c2cac617e89c12fbd704e1
Amarendra Shendkar <amarshendkar.as@gmail.com>
2021-02-11 01:18:36 +0530

    added skillribbon

---
+++ src/components/SkillRibbon.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Container } from "react-bootstrap";
+function SkillRibbon(props) {
+
+    return (
+        <Container className="jumbo-container">
+        <div className="ribbon">
+          <div>
+            <img src="pfp4-ConvertImage.jpg" alt="" />
+          </div>
+
+          <div className="jumbotext">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil
+            blanditiis, incidunt officiis saepe dolores laudantium ratione
+            perspiciatis vero omnis eveniet repudiandae ex, voluptatem error.
+            Optio aut atque sit assumenda voluptate.
+          </div>
+        </div>
+      </Container>
+    )
+}
+
+export default SkillRibbon
