079f2c2e9c9b0b09fbcf054dcdb89d59f4ed414c
Amarendra Shendkar <amarshendkar.as@gmail.com>
2021-02-16 01:56:37 +0530

    styled links

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,15 +33,16 @@ function Contact() {
             <div className="instagram">
               <label htmlFor="">Instagram : </label>
               <a href="mailto:amarshendkar.as@gmail.com">
-                <img src="https://img.icons8.com/fluent/48/000000/apple-mail.png" />
+              <img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/>
               </a>
             </div>
             <div className="twitter">
               <label>Twitter : </label>
               <a href="">
-                <img src="https://img.icons8.com/color/48/000000/linkedin.png" />
+              <img src="https://img.icons8.com/fluent/48/000000/twitter.png"/>
               </a>
             </div>
+            
           </div>
         </div>
       </div>
