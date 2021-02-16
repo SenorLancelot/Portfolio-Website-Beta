31e25928936fcb874fd2d851aa114cf6068ab3ff
Amarendra Shendkar <amarshendkar.as@gmail.com>
2021-02-16 13:09:24 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import NavbarComponent from "./components/NavbarComponent";
 import JumbotronComp from "./components/JumbotronComp";
-import HomeComponent from './components/HomeComponent';
-import ProjectsComponent from './components/ProjectsComponent';
-import Education from './components/Education';
-import Contact from './components/Contact';
+import HomeComponent from "./components/HomeComponent";
+import ProjectsComponent from "./components/ProjectsComponent";
+import Education from "./components/Education";
+import Contact from "./components/Contact";
+import { Switch, Route, Redirect, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <NavbarComponent />
-      <JumbotronComp />
-      {/* <HomeComponent/> */}
-      {/* <ProjectsComponent/> */}
-      {/* <Education/> */}
-      <Contact/>
+      <BrowserRouter>
+        <NavbarComponent />
+        <JumbotronComp />
+
+        <Switch>
+          <Route path="/home" component={HomeComponent} />
+          <Route path="/education" component={Education} />
+          <Route path="/projects" component={ProjectsComponent} />
+          <Route path="/contactme" component={Contact} />
+
+          <Redirect to="/home" />
+        </Switch>
+      </BrowserRouter>
+
+      {/* <HomeComponent />
+      <ProjectsComponent />
+      <Education />
+      <Contact /> */}
     </>
   );
 }
--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -8,23 +8,45 @@ function NavbarComponent() {
   return (
     <Navbar bg="" variant="">
       {/* <div className="navbar-inner"> */}
-        <div className="nav-left-items">
-          <Navbar.Brand href="#home">
-            
+      <div className="nav-left-items">
+        <Navbar.Brand href="#home">
+          <a href="/home">
             <h4 className="nav-name underline--magical">Amarendra Shendkar</h4>
-          </Navbar.Brand>
+          </a>
+        </Navbar.Brand>
+      </div>
+      <div className="nav-right-items">
+        <div id="navbar-button-1 nav-btn">
+          <Button variant="">
+            <a href="/education">
+              <img
+                className="nav-icon"
+                src="https://img.icons8.com/color/48/000000/saving-book.png"
+              />
+            </a>
+          </Button>
         </div>
-        <div className="nav-right-items">
-          <div id="navbar-button-1 nav-btn">
-          <Button variant=""><img className="nav-icon" src="https://img.icons8.com/fluent/48/000000/project.png"/></Button>
-          </div>
-          <div id="navbar-button-2 nav-btn">
-          <Button variant=""><img className="nav-icon" src="https://img.icons8.com/color/48/000000/saving-book.png"/></Button>
-          </div>
-          <div id="navbar-button-3 nav-btn">
-          <Button variant=""><img className="nav-icon" src="https://img.icons8.com/fluent/48/000000/phone-office.png"/></Button>
-          </div>
+        <div id="navbar-button-2 nav-btn">
+          <Button variant="">
+            <a href="/projects">
+              <img
+                className="nav-icon"
+                src="https://img.icons8.com/fluent/48/000000/project.png"
+              />
+            </a>
+          </Button>
         </div>
+        <div id="navbar-button-3 nav-btn">
+          <Button variant="">
+            <a href="/contactme">
+              <img
+                className="nav-icon"
+                src="https://img.icons8.com/fluent/48/000000/phone-office.png"
+              />
+            </a>
+          </Button>
+        </div>
+      </div>
       {/* </div> */}
     </Navbar>
   );
